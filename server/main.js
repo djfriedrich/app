c80a5c1b4f11af2a4a789853e9a7da4f55e50228
djfriedrich <47004206+djfriedrich@users.noreply.github.com>
2019-02-03 12:41:18 -0500

    Can now populate Users, Questions, Opts, & Surveys DB.

--- server/main.js
+++ server/main.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import '../imports/api/users.js';
 import '../imports/api/questions.js';
+import '../imports/api/surveys.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
